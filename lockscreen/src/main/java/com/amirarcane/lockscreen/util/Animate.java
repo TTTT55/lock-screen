d3473239f312d762d789211d4c4f926a5da45e25
Notorious <35992771+TTTT55@users.noreply.github.com>
2022-06-19 08:05:25 +0530

    Update Animate.java

--- lockscreen/src/main/java/com/amirarcane/lockscreen/util/Animate.java
+++ lockscreen/src/main/java/com/amirarcane/lockscreen/util/Animate.java
@@ -3,7 +3,7 @@ package com.amirarcane.lockscreen.util;
 import android.annotation.TargetApi;
 import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Build;
-import android.support.v7.widget.AppCompatImageView;
+import androidx.appcompat.widget.AppCompatImageView;
 
 /**
  * Created by Arcane on 7/23/2017.
